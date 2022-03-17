c334126b88937c354368997fde5e0bc212e87adc
Elien <elienferre@yahoo.fr>
2022-03-17 16:03:34 +0100

    created addConnection page and its controller

--- src/main/java/com/openclassrooms/paymybuddy/controller/TransferController.java
+++ src/main/java/com/openclassrooms/paymybuddy/controller/TransferController.java
@@ -2,6 +2,7 @@ package com.openclassrooms.paymybuddy.controller;
 
 import com.openclassrooms.paymybuddy.model.Transaction;
 import com.openclassrooms.paymybuddy.model.User;
+import com.openclassrooms.paymybuddy.model.UserBeneficiary;
 import com.openclassrooms.paymybuddy.model.utils.layout.Paged;
 import com.openclassrooms.paymybuddy.service.UserService;
 import org.apache.logging.log4j.LogManager;
@@ -10,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.security.Principal;
@@ -17,6 +20,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@RequestMapping("/transfer")
 public class TransferController {
 
   private static final Logger LOGGER = LogManager.getLogger(TransferController.class);
@@ -24,7 +28,7 @@ public class TransferController {
   @Autowired
   private UserService userService;
 
-  @GetMapping("/transfer")
+  @GetMapping
   public String getTransfer(
     @RequestParam(value = "pageNumber", required = false, defaultValue = "1") int pageNumber,
     @RequestParam(value = "size", required = false, defaultValue = "3") int size,
@@ -47,8 +51,40 @@ public class TransferController {
 
     model.addAttribute("beneficiaries", stringListOfBeneficiaries);
     model.addAttribute("transactionPages", transactionsPaged);
-    model.addAttribute("userEmail",userFromDB.getEmail());
+    model.addAttribute("userEmail", userFromDB.getEmail());
 
     return "transfer";
+  }
+
+  @GetMapping("/addConnection")
+  public String getAddConnectionPage(Principal principal, Model model) throws Exception {
+    User userFromDB = userService.getUserFromPrincipal(principal);
+    model.addAttribute("userEmail", userFromDB.getEmail());
+    model.addAttribute("email", null);
+    return "addConnection";
+  }
+
+  @PostMapping("/addConnection")
+  public String postAddConnectionPage(
+    Principal principal,
+    Model model,
+    String connectionEmail
+  ) throws Exception {
+    User userFromDB = userService.getUserFromPrincipal(principal);
+    if (!userFromDB.getEmail().equals(connectionEmail)) {
+      if (userService.existsByEmail(connectionEmail)) {
+        UserBeneficiary userBeneficiary = new UserBeneficiary();
+        userBeneficiary.setBeneficiary(
+          userService.getUserByEmail(connectionEmail).get()
+        );
+        userFromDB.addUserBeneficiary(userBeneficiary);
+        userService.save(userFromDB);
+        return "redirect:/transfer?addConnectionSuccessful";
+      }
+    }
+      model.addAttribute("errorOwnEmail", true);
+      model.addAttribute("userEmail", userFromDB.getEmail());
+      model.addAttribute("email", connectionEmail);
+    return "addConnection";
   }
 }
