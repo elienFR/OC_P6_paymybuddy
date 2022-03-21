c2ee93d74f29d710b6e1aaa81f71c22791434619
Elien <elienferre@yahoo.fr>
2022-03-21 15:25:13 +0100

    finished pay/transaction feature

--- src/main/java/com/openclassrooms/paymybuddy/controller/TransferController.java
+++ src/main/java/com/openclassrooms/paymybuddy/controller/TransferController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.transaction.Transactional;
 import java.security.Principal;
 import java.util.List;
 
@@ -48,20 +49,51 @@ public class TransferController {
     return "transfer";
   }
 
+  @Transactional
   @PostMapping("/successful")
   public String postTransferSuccessful(
     Principal principal,
     Model model,
     float amount,
+    String descriptionOfTransaction,
     String userBeneficiaryEmail
-  ){
-    if(userBeneficiaryEmail == null){
-      return "redirect:/transfer";
+  ) throws Exception {
+
+    LOGGER.info("Starting payment /transfer/successful...");
+
+    StringBuffer errorStatement = new StringBuffer();
+
+    if (userBeneficiaryEmail == null) {
+      LOGGER.warn("Beneficiary mail is null redirect to /transfer without making payment...");
+      errorStatement.append("&nullBeneficiary");
     }
 
-    System.out.println("test");
+    if(amount<=0) {
+      LOGGER.warn("Cannot perform a zero or negative money transfer.");
+      errorStatement.append("&zeroAmount");
+    }
 
-    return "transferSuccessful";
+    if(errorStatement.toString().isBlank()){
+      LOGGER.info("Initiating payment -> from : "
+        + userService.getUserFromPrincipal(principal).getEmail()
+        + " to : " + userBeneficiaryEmail);
+
+        LOGGER.info("Performing transfer of amount " + amount);
+        userService.makeATransaction(
+          userService.getUserFromPrincipal(principal),
+          userService.getUserByEmail(userBeneficiaryEmail).get(),
+          descriptionOfTransaction,
+          amount
+        );
+
+        model.addAttribute("userEmail", userService.getUserFromPrincipal(principal).getEmail());
+        model.addAttribute("userBeneficiaryEmail", userBeneficiaryEmail);
+        model.addAttribute("amount",amount);
+        model.addAttribute("currency",userService.getUserFromPrincipal(principal).getAccount().getCurrencyCode().toString());
+        return "transferSuccessful";
+      }
+
+    return "redirect:/transfer?" + errorStatement;
   }
 
   @GetMapping("/addConnection")
@@ -90,9 +122,9 @@ public class TransferController {
         return "redirect:/transfer?addConnectionSuccessful";
       }
     }
-      model.addAttribute("errorOwnEmail", true);
-      model.addAttribute("userEmail", userFromDB.getEmail());
-      model.addAttribute("email", connectionEmail);
+    model.addAttribute("errorOwnEmail", true);
+    model.addAttribute("userEmail", userFromDB.getEmail());
+    model.addAttribute("email", connectionEmail);
     return "addConnection";
   }
 }
