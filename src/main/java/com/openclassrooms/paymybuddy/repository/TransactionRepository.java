0868c624eda2bcc1a795756432e7b18e7a28ec35
Elien <elienferre@yahoo.fr>
2022-03-24 14:12:50 +0100

    Get all transaction with : in and out

--- src/main/java/com/openclassrooms/paymybuddy/repository/TransactionRepository.java
+++ src/main/java/com/openclassrooms/paymybuddy/repository/TransactionRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
-  Page<Transaction> findAllByFromAccount(Account fromAccount, Pageable page);
+  Page<Transaction> findAllByFromAccountOrToAccount(Account fromAccount, Account toAccount, Pageable page);
 }
